c5361de09fef379532ca2c02a470836fc367d13c
Mateusz Palyz <m.palyz@pilot.co>
2014-10-03 19:19:04 +0200

    #2 add Middleware class that sets the X-RateLimit-Limit header value

---
+++ lib/middleware.rb
@@ -0,0 +1,12 @@
+class Middleware
+  def initialize(app, options = {})
+    @app = app
+    @options = options
+  end
+
+  def call(env)
+    status, headers, response = @app.call env
+    headers['X-RateLimit-Limit'] = @options[:limit]
+    [status, headers, response]
+  end
+end
