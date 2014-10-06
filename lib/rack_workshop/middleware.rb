24a95f6445c7a6fa6a58e9199815b56dd754c0de
Mateusz Palyz <m.palyz@pilot.co>
2014-10-06 14:46:19 +0200

    #6 separate calls by address and store them in hash in order to separate limit for each client

--- lib/rack_workshop/middleware.rb
+++ lib/rack_workshop/middleware.rb
@@ -3,25 +3,27 @@ module RackWorkshop
     def initialize(app, options = {})
       @app = app
       @options = options
-      @calls_number = 0
+      @calls = {}
     end
 
     def call(env)
-      @timestamp = Time.now if @calls_number == 0
+      @calls[env['REMOTE_ADDR']] ||= Hash.new(0)
 
-      if Time.now - @timestamp > (@options[:reset_in] || 3600)
-        @timestamp = Time.now
-        @calls_number = 0
+      @calls[env['REMOTE_ADDR']]['timestamp'] = Time.now if @calls[env['REMOTE_ADDR']]['calls_number'] == 0
+
+      if Time.now - @calls[env['REMOTE_ADDR']]['timestamp'] > (@options[:reset_in] || 3600)
+        @calls[env['REMOTE_ADDR']]['timestamp'] = Time.now
+        @calls[env['REMOTE_ADDR']]['calls_number'] = 0
       end
 
-      @calls_number += 1
-      if @calls_number > @options[:limit]
+      @calls[env['REMOTE_ADDR']]['calls_number'] += 1
+      if @calls[env['REMOTE_ADDR']]['calls_number'] > @options[:limit]
         [429, { 'Content-Type' => 'text/html' }, "Too many Requests"]
       else
         status, headers, response = @app.call env
         headers['X-RateLimit-Limit'] = @options[:limit]
-        headers['X-RateLimit-Remaining'] = @options[:limit] - @calls_number
-        headers['X-RateLimit-Reset'] = @timestamp + 3600
+        headers['X-RateLimit-Remaining'] = @options[:limit] - @calls[env['REMOTE_ADDR']]['calls_number']
+        headers['X-RateLimit-Reset'] = @calls[env['REMOTE_ADDR']]['timestamp'] + 3600
         [status, headers, response]
       end
     end
