b86e5896ae4319770087099c982833d79c9c949a
Mateusz Palyz <m.palyz@pilot.co>
2014-10-06 14:17:09 +0200

    #5 reset the limit after one hour

--- lib/rack_workshop/middleware.rb
+++ lib/rack_workshop/middleware.rb
@@ -7,6 +7,13 @@ module RackWorkshop
     end
 
     def call(env)
+      @timestamp = Time.now if @calls_number == 0
+
+      if Time.now - @timestamp > (@options[:reset_in] || 3600)
+        @timestamp = Time.now
+        @calls_number = 0
+      end
+
       @calls_number += 1
       if @calls_number > @options[:limit]
         [429, { 'Content-Type' => 'text/html' }, "Too many Requests"]
@@ -14,6 +21,7 @@ module RackWorkshop
         status, headers, response = @app.call env
         headers['X-RateLimit-Limit'] = @options[:limit]
         headers['X-RateLimit-Remaining'] = @options[:limit] - @calls_number
+        headers['X-RateLimit-Reset'] = @timestamp + 3600
         [status, headers, response]
       end
     end
