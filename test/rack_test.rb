93b99abe2530babe208d8b09fe62721e15491637
Mateusz Palyz <m.palyz@pilot.co>
2014-10-06 10:39:27 +0200

    #1 update new test file

---
+++ test/rack_test.rb
@@ -0,0 +1,24 @@
+require 'minitest/autorun'
+require 'rack/test'
+require '../lib/middleware'
+
+class RackTest < Minitest::Test
+  include Rack::Test::Methods
+
+  def app
+    rack_app = lambda { |env| [200, { 'Content-Type' => 'text/html' }, "Hello Rack"] }
+    Middleware.new(rack_app, { limit: 100 })
+  end
+
+  def setup
+    get '/'
+  end
+
+  def test_response
+    assert last_response.ok?
+  end
+
+  def test_X_RateLimit_Limit_header
+    assert_equal 100, last_response.header['X-RateLimit-Limit']
+  end
+end
