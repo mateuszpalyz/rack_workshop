4e4602dfad9a2c51095485b046b0848195c0c9af
Mateusz Palyz <m.palyz@pilot.co>
2014-10-06 12:23:52 +0200

    #2 wrap Middleware class in RackWorkshop module

--- lib/rack_workshop/middleware.rb
+++ lib/rack_workshop/middleware.rb
@@ -1,12 +1,14 @@
-class Middleware
-  def initialize(app, options = {})
-    @app = app
-    @options = options
-  end
+module RackWorkshop
+  class Middleware
+    def initialize(app, options = {})
+      @app = app
+      @options = options
+    end
 
-  def call(env)
-    status, headers, response = @app.call env
-    headers['X-RateLimit-Limit'] = @options[:limit]
-    [status, headers, response]
+    def call(env)
+      status, headers, response = @app.call env
+      headers['X-RateLimit-Limit'] = @options[:limit]
+      [status, headers, response]
+    end
   end
 end
