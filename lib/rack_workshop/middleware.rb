e61460fa8abe5a4f7a534fe642369733990a8da6
Mateusz Palyz <m.palyz@pilot.co>
2014-10-06 12:33:17 +0200

    #3 set remaining limit header

--- lib/rack_workshop/middleware.rb
+++ lib/rack_workshop/middleware.rb
@@ -3,11 +3,14 @@ module RackWorkshop
     def initialize(app, options = {})
       @app = app
       @options = options
+      @calls_number = 0
     end
 
     def call(env)
+      @calls_number += 1
       status, headers, response = @app.call env
       headers['X-RateLimit-Limit'] = @options[:limit]
+      headers['X-RateLimit-Remaining'] = @options[:limit] - @calls_number
       [status, headers, response]
     end
   end
