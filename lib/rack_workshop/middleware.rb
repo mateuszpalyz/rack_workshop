0f915723feb21df01f63fa76811e321589aeceb0
Mateusz Palyz <m.palyz@pilot.co>
2014-10-06 12:51:03 +0200

    #4 check if the limit is exceeded and response with 429

--- lib/rack_workshop/middleware.rb
+++ lib/rack_workshop/middleware.rb
@@ -8,10 +8,14 @@ module RackWorkshop
 
     def call(env)
       @calls_number += 1
-      status, headers, response = @app.call env
-      headers['X-RateLimit-Limit'] = @options[:limit]
-      headers['X-RateLimit-Remaining'] = @options[:limit] - @calls_number
-      [status, headers, response]
+      if @calls_number > @options[:limit]
+        [429, { 'Content-Type' => 'text/html' }, "Too many Requests"]
+      else
+        status, headers, response = @app.call env
+        headers['X-RateLimit-Limit'] = @options[:limit]
+        headers['X-RateLimit-Remaining'] = @options[:limit] - @calls_number
+        [status, headers, response]
+      end
     end
   end
 end
