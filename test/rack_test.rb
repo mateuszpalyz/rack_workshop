f4e317acc42ad03d9d730c2edfcd002430bcecf1
Mateusz Palyz <m.palyz@pilot.co>
2015-03-06 13:56:46 +0100

    #7 remove get '/' from tests setup

--- test/rack_test.rb
+++ test/rack_test.rb
@@ -15,34 +15,36 @@ class RackTest < Minitest::Test
 
   def setup
     Timecop.freeze(Time.now)
-    get '/'
   end
 
   def test_response
+    get '/'
     assert last_response.ok?
   end
 
   def test_X_RateLimit_Limit_header
+    get '/'
     assert_equal 100, last_response.header['X-RateLimit-Limit']
   end
 
   def test_X_RateLimit_Remaining_header
     3.times { get '/' }
-    assert_equal 96, last_response.header['X-RateLimit-Remaining']
+    assert_equal 97, last_response.header['X-RateLimit-Remaining']
   end
 
   def test_exceeding_limit_header
-    100.times { get '/' }
+    101.times { get '/' }
     assert_equal 429, last_response.status
     assert_equal 'Too many Requests', last_response.body
   end
 
   def test_limit_header
+    get '/'
     assert_equal Time.now + 3600, last_response.header['X-RateLimit-Reset']
   end
 
   def test_limit_header_reset_after_one_hour
-    100.times { get '/' }
+    101.times { get '/' }
     assert_equal 429, last_response.status
     Timecop.freeze(Time.now + 3601)
     get '/'
@@ -58,5 +60,6 @@ class RackTest < Minitest::Test
 
   def teardown
     Timecop.return
+    @app = nil
   end
 end
