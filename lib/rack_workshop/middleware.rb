bc6f467b5aa802498c01dcff66b0313c03b509d1
Mateusz Palyz <m.palyz@pilot.co>
2015-03-06 13:53:48 +0100

    #7 identify clients either by ip address or by custom auth supplied from block

--- lib/rack_workshop/middleware.rb
+++ lib/rack_workshop/middleware.rb
@@ -8,26 +8,40 @@ module RackWorkshop
     end
 
     def call(env)
-      @calls[env['REMOTE_ADDR']] ||= Hash.new(0)
+      set_client_identifier(env)
 
-      set_timestamp_for_first_call(env)
-      reset_limit(env) if reset_limit?(env)
-      increase_calls_number(env)
+      if @client_identifier
+        @calls[@client_identifier] ||= Hash.new(0)
 
-      limit_exceeded?(env) ? bad_response : good_response(env)
+        set_timestamp_for_first_call(env)
+        reset_limit(env) if reset_limit?(env)
+        increase_calls_number(env)
+
+        limit_exceeded?(env) ? bad_response : good_response(env)
+      else
+        basic_response(env)
+      end
+    end
+
+    def set_client_identifier(env)
+      @client_identifier = @block ? custom_auth(env) : env['REMOTE_ADDR']
+    end
+
+    def custom_auth(env)
+      @block.call(env)
     end
 
     def limit_exceeded?(env)
-      @calls[env['REMOTE_ADDR']]['calls_number'] > @options[:limit]
+      @calls[@client_identifier]['calls_number'] > @options[:limit]
     end
 
     def reset_limit?(env)
-      Time.now - @calls[env['REMOTE_ADDR']]['timestamp'] > (@options[:reset_in] || 3600)
+      Time.now - @calls[@client_identifier]['timestamp'] > (@options[:reset_in] || 3600)
     end
 
     def reset_limit(env)
-      @calls[env['REMOTE_ADDR']]['timestamp'] = Time.now
-      @calls[env['REMOTE_ADDR']]['calls_number'] = 0
+      @calls[@client_identifier]['timestamp'] = Time.now
+      @calls[@client_identifier]['calls_number'] = 0
     end
 
     def bad_response
@@ -35,19 +49,23 @@ module RackWorkshop
     end
 
     def good_response(env)
-      status, headers, response = @app.call env
+      status, headers, response = basic_response env
       headers['X-RateLimit-Limit'] = @options[:limit]
-      headers['X-RateLimit-Remaining'] = @options[:limit] - @calls[env['REMOTE_ADDR']]['calls_number']
-      headers['X-RateLimit-Reset'] = @calls[env['REMOTE_ADDR']]['timestamp'] + 3600
+      headers['X-RateLimit-Remaining'] = @options[:limit] - @calls[@client_identifier]['calls_number']
+      headers['X-RateLimit-Reset'] = @calls[@client_identifier]['timestamp'] + 3600
       [status, headers, response]
     end
 
+    def basic_response(env)
+      @app.call env
+    end
+
     def increase_calls_number(env)
-      @calls[env['REMOTE_ADDR']]['calls_number'] += 1
+      @calls[@client_identifier]['calls_number'] += 1
     end
 
     def set_timestamp_for_first_call(env)
-      @calls[env['REMOTE_ADDR']]['timestamp'] = Time.now if @calls[env['REMOTE_ADDR']]['calls_number'] == 0
+      @calls[@client_identifier]['timestamp'] = Time.now if @calls[@client_identifier]['calls_number'] == 0
     end
   end
 end
